04fec4d14c14002128faad7d8586ccd7e9a6f829
RicardoMath <gabo_math@hotmail.com>
2014-06-04 12:42:53 -0430

    Create while.php
    se recibe la cantidad de veces que se repetira el msj.
    Su empresa me necesita.!

---
+++ while.php
@@ -0,0 +1,10 @@
+<?php
+
+$i=0;
+while ($i < $_GET["numero"] ) {//se recibe con el metodo GET la cantidad de veces que se repetira el msj y que sera la condicion de parada del while
+	echo "Soy el Mejor B-)"."<br>";
+$i++;
+}
+echo "Se acabo";
+
+?>
